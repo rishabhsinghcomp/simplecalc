3a9af9549a2b4cf03d82e9d24411985759958705
rishabhsinghcomp <91147973+rishabhsinghcomp@users.noreply.github.com>
2022-04-06 21:04:44 +0530

    Update urls.py

--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -17,8 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('',include('firstapp.urls'))
-    ,path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('calculator/',include('calculator.urls'))
     
 ]
